5704a7cb4127cc1fa83a7ce8bdb3c0c883091aa9
Joe Wilson <andreas.w@barantum.com>
2023-02-06 22:42:39 +0700

    first commit on simple rest spring boot

---
+++ src/test/java/com/example/simplerestapispringboot/SimpleRestApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerestapispringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
