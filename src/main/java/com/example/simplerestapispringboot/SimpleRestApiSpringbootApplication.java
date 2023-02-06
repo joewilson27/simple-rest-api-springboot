a989ac7b2bae94df9f78ca4e9ff85f4e9cd39d1b
Joe Wilson <andreas.w@barantum.com>
2023-02-06 22:48:59 +0700

    third commit (not yet ready)

---
+++ src/main/java/com/example/simplerestapispringboot/SimpleRestApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.simplerestapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestApiSpringbootApplication.class, args);
+	}
+
+}
